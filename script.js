d8e95bad4ab5bdb74ba782c15c0d26d046d71d2a
josehenriquedasilva <josehenrique60t@gmail.com>
2025-03-05 10:29:55 -0300

    Finalizado

--- script.js
+++ script.js
@@ -5,16 +5,3 @@ function clickmenu() {
         menu2.style.display = "block";   
     }
 }
-
-const img1 = document.querySelector("div#p1");
-
-img1.addEventListener('mouseenter', entrou)
-img1.addEventListener('mouseout', saiu)
-
-function entrou() {
-    img1.style.transform = 'translate(-3px, -3px)';
-}
-
-function saiu() {
-    img1.style.transform = 'translate(0px, 0px)';
-}
