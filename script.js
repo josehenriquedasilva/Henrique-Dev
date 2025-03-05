635b9cd3d2f3406988e750b5249accc312887801
josehenriquedasilva <josehenrique60t@gmail.com>
2025-03-05 15:33:36 -0300

    Atualização

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function clickmenu() {
     if (menu2.style.display == "block") {
         menu2.style.display = "none";
+        document.querySelector('#icone').src = 'imagens/menu_white_36dp.svg';
     } else {
-        menu2.style.display = "block";   
+        menu2.style.display = "block"; 
+        document.querySelector('#icone').src = 'imagens/close_white_36dp.svg';
     }
 }
